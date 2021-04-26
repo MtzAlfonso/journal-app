af86a640982bc6c8595a9aae8902d5d88aa4f2c0
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-04-26 01:17:59 -0500

    Estilos en el login

---
+++ src/components/auth/LoginScreen.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { GoogleButton } from '../ui/GoogleButton';
+
+export const LoginScreen = () => {
+  return (
+    <>
+      <h3 className="auth__title">Login</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="email"
+          placeholder="email@example.com"
+          name="email"
+          autocomplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button className="btn btn-primary btn-block mt-3" type="submit">
+          Login
+        </button>
+        <div className="auth__social-networks">
+          <p>Login with social networks</p>
+          <GoogleButton />
+        </div>
+        <Link to="/auth/register">Create new account</Link>
+      </form>
+    </>
+  );
+};
