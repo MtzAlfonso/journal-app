e684f1dae444b05b1ce749c433b5c587861254bc
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-04-28 03:30:35 -0500

    Estilos para sidebar y notas

---
+++ src/components/journal/NothingSelected.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NothingSelected = () => {
+  return (
+    <div className="nothing__main-content">
+      <p>
+        Select something <br /> create an entry
+      </p>
+      <i className="far fa-star fa-3x"></i>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { JournalApp } from './JournalApp';
 
-// import './styles/normalize.css'
+import './styles/normalize.css'
 import './styles/styles.scss';
 
 ReactDOM.render(<JournalApp />, document.getElementById('root'));
